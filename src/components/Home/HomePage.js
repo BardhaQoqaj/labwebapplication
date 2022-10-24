06382df152abfdf954b2e73b046342c5ab10dd8c
bardha cocaj <fc52610@ubt-uni.net>
2022-10-24 13:49:19 +0200

    homee

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,62 @@
+import React from "react";
+import './home.css';
+
+function HomePage() {
+  return (
+  <div className="bg">
+
+    <div className="home-container">
+      
+      <h1 className="plans">FIND ALL YOUR FAVORITE BOOKS HERE!</h1>
+      <h4 className="goal">Our goal is for you to be closer to books than ever before</h4>
+
+      <div className="box-1">
+          <p><br/>taking into account the difficulties that arise due to the distance of the libraries</p>
+          <p>we bring the library to your hand</p>
+          <button><a href="#about">LEARN MORE ABOUT US!</a></button>
+      </div>
+
+      <h2 className="preparations">PREPARATIONS:</h2>
+      <p className="steps">Steps you should follow</p>
+      
+      <div className="box-2">
+        <ol>
+          <a href="#box1"><li>Research!</li></a> 
+          <a href="#box2"><li>Find the category you like the most</li></a>
+          <a href="#box3"><li>Plan your free time to read !</li></a>
+          <a href="#box4"><li>Start reading</li></a>
+        </ol>
+      </div>
+      </div>
+
+      <div className="box-3" id="box1">
+        <div className="box-container">
+          <h3 className="text">Read</h3>
+          <p className="text">Exactly What We're Helping You With Right Now !<br/>
+          By using our website you will see how useful it will be for your time and mind<br/>
+          We're always here for you<br/>
+          Need more help ?</p>
+          <button>Contact Us</button>
+        </div>
+
+        <div className="box-container">
+        <p className="text">Details of our Research:</p>
+        <button>View Details</button>
+      </div>
+      </div>
+
+      <div className="box-4">
+      <h2 className="text">What You Enjoy?</h2>
+
+       <p>You can find yourself in any of the many categories that our website has</p>
+      </div>
+
+      <div className="box-5">
+        <p className="text">Get Started Right Now !</p>
+        <button>SIGN UP</button>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
